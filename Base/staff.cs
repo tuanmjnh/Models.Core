4ed3b3d8e4e95716fe218168b2ca0f56637afbe0
Tuấn Hin <minhtuan200990@gmail.com>
2018-08-23 17:26:06 +0700

    tm

---
+++ Base/staff.cs
@@ -0,0 +1,16 @@
+namespace Models.Core {
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.ComponentModel.DataAnnotations;
+    using System;
+
+    [Table("staff")]
+    public partial class staff {
+        public Guid id { get; set; }
+
+        public Guid? user_id { get; set; }
+
+        [StringLength(255)]
+        public string title { get; set; }
+    }
+}
