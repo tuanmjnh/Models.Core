e00cd9e256706992cd8e6c5fb588d0e43cff8d61
Tuấn Hin <minhtuan200990@gmail.com>
2018-09-20 09:39:49 +0700

    tm

--- Base/QueryString.cs
+++
@@ -1,7 +0,0 @@
-namespace Models.Core {
-    using System;
-    public partial class QueryString {
-        public string filter { get; set; }
-        public int flag { get; set; }
-    }
-}
--- Base/QuickEdit.cs
+++
@@ -1,8 +0,0 @@
-namespace Models.Core {
-    using System;
-    public partial class QuickEdit {
-        public string id { get; set; }
-        public string value { get; set; }
-        public string column { get; set; }
-    }
-}
