95b7fee014762fb05e7d0cb5bfeca031d8393b36
Tuấn Hin <minhtuan200990@gmail.com>
2018-09-20 09:39:51 +0700

    tm

---
+++ Base/GroupItem.cs
@@ -0,0 +1,37 @@
+namespace Models.Core {
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.ComponentModel.DataAnnotations;
+    using System;
+    [Table("group_item")]
+    public partial class GroupItem {
+        public Guid id { get; set; }
+
+        [StringLength(128)]
+        public string app_key { get; set; }
+
+        public Guid? group_id { get; set; }
+
+        public Guid? item_id { get; set; }
+
+        public DateTime? started_at { get; set; }
+
+        public DateTime? ended_at { get; set; }
+
+        public int? orders { get; set; }
+
+        [StringLength(128)]
+        public string created_by { get; set; }
+
+        public DateTime? created_at { get; set; }
+
+        [StringLength(128)]
+        public string updated_by { get; set; }
+
+        public DateTime? updated_at { get; set; }
+
+        public int? flag { get; set; }
+
+        public string extras { get; set; }
+    }
+}
---
+++ Base/QuickEdit.cs
@@ -0,0 +1,8 @@
+namespace Models.Core {
+    using System;
+    public partial class QuickEdit {
+        public string id { get; set; }
+        public string value { get; set; }
+        public string column { get; set; }
+    }
+}
