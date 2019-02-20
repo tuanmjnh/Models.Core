9d2ee90a37b6d9b4174c56f1ebb17e648d76d3ae
tuanmjnh <minhtuan200990@gmail.com>
2019-02-20 23:14:27 +0700

    tm

--- Base/LanguageItems.cs
+++ Base/LanguageItems.cs
@@ -6,11 +6,12 @@
     [Table("language_items")]
     public partial class LanguageItems {
         [Dapper.Contrib.Extensions.ExplicitKey]
-        public long id { get; set; }
+        // public long id { get; set; }
         public string lang_code { get; set; }
-        public string module_code { get; set; }
-        public string key { get; set; }
-        public string value { get; set; }
+        public string lang_data { get; set; }
+        // public string module_code { get; set; }
+        // public string key { get; set; }
+        // public string value { get; set; }
         // public int orders { get; set; }
         // public string created_by { get; set; }
         // public DateTime? created_at { get; set; }
