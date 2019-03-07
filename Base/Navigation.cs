8033881304eeb59123ab1bad98e5ad9d0726235e
tuanmjnh <minhtuan200990@gmail.com>
2019-03-07 23:00:01 +0700

    tm

--- Base/Navigation.cs
+++ Base/Navigation.cs
@@ -3,19 +3,20 @@
     using System.ComponentModel.DataAnnotations;
     using System;
 
-    [Table("modules")]
-    public partial class Modules {
-        [Dapper.Contrib.Extensions.ExplicitKey]
-        public string id { get; set; }
-        public string code { get; set; }
+    [Table("navigation")]
+    public partial class Navigation {
+        [Dapper.Contrib.Extensions.Key]
+        public int id { get; set; }
+        public string dependent { get; set; }
+        // public int parent_id { get; set; }
+        // public string parents { get; set; }
+        public int levels { get; set; }
         public string title { get; set; }
         public string icon { get; set; }
         public string image { get; set; }
         public string url { get; set; }
-        public string permissions { get; set; }
         public int orders { get; set; }
         public string descs { get; set; }
-        public string contents { get; set; }
         public string created_by { get; set; }
         public DateTime? created_at { get; set; }
         public string updated_by { get; set; }
