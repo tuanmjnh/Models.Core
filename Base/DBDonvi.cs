11ba8aba87dd92a3c345de046a85bad2972347e2
tuanmjnh <minhtuan200990@gmail.com>
2019-04-05 00:02:10 +0700

    tm

---
+++ Base/DBDonvi.cs
@@ -0,0 +1,16 @@
+﻿namespace Models.Core {
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.ComponentModel.DataAnnotations;
+    using System;
+
+    [Table("db_donvi")]
+    public partial class DBDonvi {
+        [Dapper.Contrib.Extensions.ExplicitKey]
+        public int ma_dvi { get; set; }
+        public int donvi_id { get; set; }
+        public int donvi_ql_id { get; set; }
+        public string ten_donvi { get; set; }
+        public string ten_donvi_ql { get; set; }
+        public string stk { get; set; }
+    }
+}
