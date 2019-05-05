c41313227bb66ac9f9410999484d6567de7f872c
tuanmjnh <minhtuan200990@gmail.com>
2019-05-05 18:28:54 +0700

    tm

---
+++ Base/APP_KEY.cs
@@ -0,0 +1,15 @@
+﻿namespace Models.Core
+{
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.ComponentModel.DataAnnotations;
+    using System;
+
+    [Table("app_key")]
+    public partial class APP_KEY
+    {
+        [Dapper.Contrib.Extensions.Key]
+        public string app_key { get; set; }
+        public string title { get; set; }
+        public int flag { get; set; }
+    }
+}
